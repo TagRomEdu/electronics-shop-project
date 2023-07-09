eeab641d7cee7a6a0de9565d7d138fb99d879903
TagRomEdu <qwertystyles@gmail.com>
2023-07-09 21:17:03 +0500

    created tests for apply_discount, calculate_total_price and Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+ex1 = Item("name", 10, 5)
+
+
+def test_Item():
+    assert ex1.name == "name"
+    assert ex1.price == 10
+    assert ex1.quantity == 5
+    assert len(Item.all) == 1
+
+
+def test_calculate_total_price():
+    assert ex1.calculate_total_price() == 50
+
+
+def test_apply_discount():
+    ex1.pay_rate = 2
+    ex1.apply_discount()
+    assert ex1.price == 20
