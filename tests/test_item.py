5cbed3e131f4a1522b8e601a8c0547612a66e01f
TagRomEdu <qwertystyles@gmail.com>
2023-07-10 12:48:59 +0500

    test_item.py updated with test for instantiate_from_csv() and string_to_number()

--- tests/test_item.py
+++ tests/test_item.py
@@ -19,3 +19,11 @@ def test_apply_discount():
     ex1.pay_rate = 2
     ex1.apply_discount()
     assert ex1.price == 20
+
+
+def test_instantiate_from_csv():
+    assert isinstance(Item.instantiate_from_csv(), list)
+
+
+def test_string_to_number():
+    assert Item.string_to_number('8.1234') == 8
