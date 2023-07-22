c57612675f76191a567ce3004154e87722b89c6a
TagRomEdu <qwertystyles@gmail.com>
2023-07-22 13:29:28 +0500

    overriding method repr for Phone

--- src/phone.py
+++ src/phone.py
@@ -1,7 +1,13 @@
-from item import Item
+from src.item import Item
 
 
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+phone1 = Phone("iPhone 14", 120_000, 5, 2)
+print(repr(phone1))
