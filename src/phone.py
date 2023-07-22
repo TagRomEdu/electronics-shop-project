8922ec3729c49547c6b07e4c039f4e53f12aea4b
TagRomEdu <qwertystyles@gmail.com>
2023-07-22 13:51:04 +0500

    added getter and setter for number of sim

--- src/phone.py
+++ src/phone.py
@@ -4,7 +4,17 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
+
+    @property
+    def number_of_sim(self) -> int:
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim) -> None:
+        if number_of_sim > 0:
+            self.__number_of_sim = number_of_sim
+        raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
