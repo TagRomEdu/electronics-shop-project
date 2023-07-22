df6fba7c78cd9871a8a091588a4382198bd9120c
TagRomEdu <qwertystyles@gmail.com>
2023-07-22 13:04:35 +0500

    added class Phone(Item) and init this with new attr - number of sim

---
+++ src/phone.py
@@ -0,0 +1,7 @@
+from item import Item
+
+
+class Phone(Item):
+    def __init__(self, name, price, quantity, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
