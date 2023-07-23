f6cd3a0635e0b8b0f4d9aec428a9100761bab270
TagRomEdu <qwertystyles@gmail.com>
2023-07-23 18:04:22 +0500

    added docstrings

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,7 +2,9 @@ from src.item import Item
 
 
 class MixinChange:
+
     """Миксин класс для реализации смены языка клавиатуры"""
+
     def __init__(self):
         self.__language = 'EN'
 
@@ -11,11 +13,18 @@ class MixinChange:
         return self.__language
 
     def change_lang(self):
+
+        """Возвращает экземпляр класса с изменной раскладкой языка.
+        Так нужно, чтобы можно было применять метод несколько раз: kb.change_lang().change_lang()"""
+
         self.__language = 'RU' if self.__language == 'EN' else 'EN'
         return self
 
 
 class Keyboard(Item, MixinChange):
+
+    """Класс для кдавиатур"""
+
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
         MixinChange.__init__(self)
