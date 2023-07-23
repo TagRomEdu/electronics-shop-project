25fb712aa86200e201b8fdc45fe118499bfbdf3f
TagRomEdu <qwertystyles@gmail.com>
2023-07-23 16:26:34 +0500

    added Mixin class for switch lang

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,18 +3,28 @@ from src.item import Item
 
 class MixinChange:
     """Миксин класс для реализации смены языка клавиатуры"""
+    lang = 'EN'
+
+    def __init__(self):
+        self.language = self.lang
 
     def change_lang(self):
-        if self.language == 'RU':
-            self.language == 'EN'
+        if self.language == 'EN':
+            self.language = 'RU'
             return self.language
+        self.language = 'EN'
+        return self.language
 
 
 class Keyboard(Item, MixinChange):
-    def __init__(self, name, price, quantity, language):
+    def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
-        self.language = language
+        self.__language = 'EN'
 
 
 kb = Keyboard('Dark Project KD87A', 9600, 5)
-str(kb) == "Dark Project KD87A"
+print(str(kb))
+print(kb.__dict__)
+print(kb.change_lang())
+print(kb.__dict__)
+print(kb.change_lang())
